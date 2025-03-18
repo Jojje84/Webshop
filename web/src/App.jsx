f895610e8d114cbfd53b2b0576ff020b7c6830bb
Jorge <68773604+Jojje84@users.noreply.github.com>
2025-03-18 12:02:23 +0000

    navbar update

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,25 @@
+import styled, { createGlobalStyle } from "styled-components"
 import Home from "./pages/Home"
 
+const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+* {
+box-sizing: border-box;
+}
+`
+
 const App = () => {
-  return <Home/>
+  return (
+    <>
+      <GlobalStyle />
+      <Home />
+    </>
+  )
 }
 
 export default App
--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
     <div>
-        <Navbar />
+      <Banner />
+      <Navbar />
     </div>
-  ) 
+  )
 }
 
 export default Home
