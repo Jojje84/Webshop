921229e2702de650ce1995cacaa8333c5738f865
Jorge <68773604+Jojje84@users.noreply.github.com>
2025-04-01 10:04:34 +0000

    update products

--- web/src/data/products.js
+++ web/src/data/products.js
@@ -92,6 +92,51 @@ export const products = [
   },
   {
     id: 11,
+    name: 'Anime Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/anime1.png',
+    category: 'Anime',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 12,
+    name: 'Anime Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/anime2.png',
+    category: 'Anime',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 13,
+    name: 'Anime Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/anime3.png',
+    category: 'Anime',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 14,
+    name: 'Anime Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/anime4.png',
+    category: 'Still Life',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 15,
+    name: 'Anime Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/anime5.png',
+    category: 'Anime',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 16,
     name: 'Citys Painting',
     description: '',
     price: 1000,
@@ -100,7 +145,7 @@ export const products = [
     stock: 10 // Antal produkter i lager
   },
   {
-    id: 12,
+    id: 17,
     name: 'Citys Painting',
     description: '',
     price: 1000,
@@ -109,7 +154,7 @@ export const products = [
     stock: 10 // Antal produkter i lager
   },
   {
-    id: 13,
+    id: 18,
     name: 'Citys Painting',
     description: '',
     price: 1000,
@@ -118,7 +163,7 @@ export const products = [
     stock: 10 // Antal produkter i lager
   },
   {
-    id: 14,
+    id: 19,
     name: 'Citys Painting',
     description: '',
     price: 1000,
@@ -127,13 +172,285 @@ export const products = [
     stock: 10 // Antal produkter i lager
   },
   {
-    id: 15,
+    id: 20,
     name: 'Citys Painting',
     description: '',
     price: 1000,
     image: '/assets/images/products/abstract/citys5.png',
     category: 'Citys',
     stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 21,
+    name: 'Interactive Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/interactive1.png',
+    category: 'Interactive',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 22,
+    name: 'Interactive Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/interactive2.png',
+    category: 'Interactive',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 23,
+    name: 'Interactive Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/interactive3.png',
+    category: 'Interactive',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 24,
+    name: 'Interactive Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/interactive4.png',
+    category: 'Interactive',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 25,
+    name: 'Interactive Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/interactive5.png',
+    category: 'Interactive',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 26,
+    name: 'Mask Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/mask1.png',
+    category: 'Mask',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 27,
+    name: 'Mask Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/mask2.png',
+    category: 'Mask',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 28,
+    name: 'Mask Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/mask3.png',
+    category: 'Mask',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 29,
+    name: 'Mask Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/mask4.png',
+    category: 'Mask',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 30,
+    name: 'Mask Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/mask5.png',
+    category: 'Mask',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 31,
+    name: 'Nature Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/nature1.png',
+    category: 'Nature',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 32,
+    name: 'Nature Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/nature2.png',
+    category: 'Nature',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 33,
+    name: 'Nature Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/nature3.png',
+    category: 'Nature',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 34,
+    name: 'Nature Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/nature4.png',
+    category: 'Nature',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 35,
+    name: 'Nature Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/nature5.png',
+    category: 'Nature',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 36,
+    name: 'People Portrait',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/portrait1.png',
+    category: 'Portrait',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 37,
+    name: 'People Portrait',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/portrait2.png',
+    category: 'Portrait',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 38,
+    name: 'People Portrait',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/portrait3.png',
+    category: 'Portrait',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 39,
+    name: 'People Portrait',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/portrait4.png',
+    category: 'Portrait',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 40,
+    name: 'People Portrait',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/portrait5.png',
+    category: 'Portrait',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 41,
+    name: 'Retro Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/retro1.png',
+    category: 'Retro',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 42,
+    name: 'Retro Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/retro2.png',
+    category: 'Retro',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 43,
+    name: 'Retro Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/retro3.png',
+    category: 'Retro',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 44,
+    name: 'Retro Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/retro4.png',
+    category: 'Retro',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 45,
+    name: 'Retro Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/retro5.png',
+    category: 'Retro',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 46,
+    name: 'Warrior Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/warrior1.png',
+    category: 'Warrior',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 47,
+    name: 'Warrior Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/warrior2.png',
+    category: 'Warrior',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 48,
+    name: 'Warrior Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/warrior3.png',
+    category: 'Warrior',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 49,
+    name: 'Warrior Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/warrior4.png',
+    category: 'Warrior',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 50,
+    name: 'Warrior Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/warrior5.png',
+    category: 'Warrior',
+    stock: 10 // Antal produkter i lager
   }
+  
+  
   // Lägg till fler produkter här
 ];
