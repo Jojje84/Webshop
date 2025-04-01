7ab5e28db6e1338a4c2283a8b4b29b5c54907292
Jorge <68773604+Jojje84@users.noreply.github.com>
2025-04-01 09:34:48 +0000

    update products

--- web/src/data/products.js
+++ web/src/data/products.js
@@ -44,6 +44,96 @@ export const products = [
     image: '/assets/images/products/abstract/abstract5.png',
     category: 'Abstract',
     stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 6,
+    name: 'Animal Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/animal1.png',
+    category: 'Animal',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 7,
+    name: 'Animal Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/animal2.png',
+    category: 'Animal',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 8,
+    name: 'Animal Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/animal3.png',
+    category: 'Animal',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 9,
+    name: 'Animal Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/animal4.png',
+    category: 'Animal',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 10,
+    name: 'Animal Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/animal5.png',
+    category: 'Animal',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 11,
+    name: 'Citys Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/citys1.png',
+    category: 'Citys',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 12,
+    name: 'Citys Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/citys2.png',
+    category: 'Citys',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 13,
+    name: 'Citys Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/citys3.png',
+    category: 'Citys',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 14,
+    name: 'Citys Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/citys4.png',
+    category: 'Citys',
+    stock: 10 // Antal produkter i lager
+  },
+  {
+    id: 15,
+    name: 'Citys Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/citys5.png',
+    category: 'Citys',
+    stock: 10 // Antal produkter i lager
   }
   // Lägg till fler produkter här
 ];
