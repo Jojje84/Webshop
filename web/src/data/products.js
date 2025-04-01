6b668c819caeee570a47d85d456e6f5985aaf1a7
Jorge <68773604+Jojje84@users.noreply.github.com>
2025-04-01 08:57:42 +0000

    updatea abstract

--- web/src/data/products.js
+++ web/src/data/products.js
@@ -1,31 +1,49 @@
-// src/data/products.js
+
 export const products = [
   {
     id: 1,
     name: 'Abstract Painting',
     description: '',
-    price: 100,
+    price: 1000,
     image: '/assets/images/products/abstract/abstract1.png',
     category: 'Abstract',
     stock: 10 // Antal produkter i lager
   },
   {
     id: 2,
-    name: 'Hoodie',
-    description: 'En mjuk och varm hoodie, perfekt för kalla dagar.',
-    price: 39.99,
-    image: '/images/hoodie.jpg',
-    category: 'Clothing',
-    stock: 5 // Antal produkter i lager
+    name: 'Abstract Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/abstract2.png',
+    category: 'Abstract',
+    stock: 10 // Antal produkter i lager
   },
   {
     id: 3,
-    name: 'Mug',
-    description: 'En fin mugg för morgonkaffet.',
-    price: 9.99,
-    image: '/images/mug.jpg',
-    category: 'Accessories',
-    stock: 20 // Antal produkter i lager
+    name: 'Abstract Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/abstract3.png',
+    category: 'Abstract',
+    stock: 10 // Antal produkter i lager
+  }
+  {
+    id: 4,
+    name: 'Abstract Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/abstract4.png',
+    category: 'Abstract',
+    stock: 10 // Antal produkter i lager
+  }
+  {
+    id: 5,
+    name: 'Abstract Painting',
+    description: '',
+    price: 1000,
+    image: '/assets/images/products/abstract/abstract5.png',
+    category: 'Abstract',
+    stock: 10 // Antal produkter i lager
   }
   // Lägg till fler produkter här
 ];
