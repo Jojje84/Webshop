c77997aff7ed465bbb6f628ca15e2afcf8ee14fc
Jorge <68773604+Jojje84@users.noreply.github.com>
2025-04-01 10:27:45 +0000

    update categorys images

--- web/src/data/categories.js
+++ web/src/data/categories.js
@@ -1,6 +1,13 @@
 import abstractImg from '../assets/images/categories/abstract.png';
 import animalsImg from '../assets/images/categories/animal.png';
+import animeImg from '../assets/images/categories/anime.png';
 import citysImg from '../assets/images/categories/citys.png';
+import interactiveImg from '../assets/images/categories/interactive.png';
+import maskImg from '../assets/images/categories/mask.png';
+import natureImg from '../assets/images/categories/nature.png';
+import portraitImg from '../assets/images/categories/portrait.png';
+import retroImg from '../assets/images/categories/retro.png';
+import warriorImg from '../assets/images/categories/warrior.png';
 import demoImg from '../assets/images/categories/demo.png';
 
 export const categories = [
@@ -13,8 +20,36 @@ export const categories = [
       image: animalsImg,
     },
     {
+      name: 'Anime',
+      image: animeImg,
+    },
+    {
       name: 'Citys',
       image: citysImg,
+    },
+    {
+      name: 'Interactive',
+      image: interactiveImg,
+    },
+    {
+      name: 'Mask',
+      image: maskImg,
+    },
+    {
+      name: 'Nature',
+      image: natureImg,
+    },
+    {
+      name: 'Portrait',
+      image: portraitImg,
+    },
+    {
+      name: 'Retro',
+      image: retroImg,
+    },
+    {
+      name: 'Warrior',
+      image: warriorImg,
     },
     {
         name: 'Demo',
