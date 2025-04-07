7a6d1637c624499dcb337d5d152c782848d48ee6
Jorge Avila <jorgeavilas@icloud.com>
2025-04-07 11:23:36 +0200

    update pd info

--- web/src/data/products.js
+++ web/src/data/products.js
@@ -1,10 +1,9 @@
-
-
 export const products = [
   {
     id: 1,
-    name: 'Abstract Painting',
-    description: '',
+    name: 'Abstract Flow – The Colorful Fusion',
+    description:
+      'A dynamic and fluid fusion of vibrant colors moving in perfect harmony. A hypnotic abstract design that creates a sense of movement and energy.',
     price: 1000,
     image: 'abstract1.png',
     category: 'Abstract',
@@ -12,8 +11,9 @@ export const products = [
   },
   {
     id: 2,
-    name: 'Abstract Painting',
-    description: '',
+    name: 'Celestial Spiral – The Cosmic Whirl',
+    description:
+      ' A swirling spiral of light and dark colors spinning in an endless cycle. This abstract animation captures the feeling of cosmic motion and the eternal flow of time.',
     price: 1000,
     image: 'abstract2.png',
     category: 'Abstract',
@@ -21,8 +21,9 @@ export const products = [
   },
   {
     id: 3,
-    name: 'Abstract Painting',
-    description: '',
+    name: 'Cosmic Harmony – Flow of Energy',
+    description:
+      'An energy-charged design where colors flow freely, creating movement and a sense of a universe in constant change. A visual expression of the power and flow of energy.',
     price: 1000,
     image: 'abstract3.png',
     category: 'Abstract',
@@ -30,8 +31,9 @@ export const products = [
   },
   {
     id: 4,
-    name: 'Abstract Painting',
-    description: '',
+    name: 'Electric Winds – Surge of Motion',
+    description:
+      'An intense, abstract artwork where strong color beams collide and form complex patterns, as if controlled by electric forces. This image evokes feelings of speed and motion',
     price: 1000,
     image: 'abstract4.png',
     category: 'Abstract',
@@ -39,8 +41,9 @@ export const products = [
   },
   {
     id: 5,
-    name: 'Abstract Painting',
-    description: '',
+    name: 'Radiant Pulse – Vibrant Energy',
+    description:
+      'A powerful design where bright colors vibrate and pulse as if spreading energy through space. This artwork creates a sense of constant movement and pulsating power.',
     price: 1000,
     image: 'abstract5.png',
     category: 'Abstract',
@@ -48,8 +51,9 @@ export const products = [
   },
   {
     id: 6,
-    name: 'Animal Painting',
-    description: '',
+    name: 'Mystic Creature – The Enchanted Beast',
+    description:
+      ' A mesmerizing, glowing creature with fluid, mystical features. This mythical animal has a captivating aura, drawing the eye with its enchanting movements and vibrant colors.',
     price: 1000,
     image: 'animal1.png',
     category: 'Animals',
@@ -57,8 +61,9 @@ export const products = [
   },
   {
     id: 7,
-    name: 'Animal Painting',
-    description: '',
+    name: 'Aquatic Dream – The Flowing Guardian',
+    description:
+      'A magical, water-inspired being that flows gracefully through its environment. Its translucent, glowing form evokes a sense of peace and fluidity, as if it’s both part of the water and a guardian of the deep.',
     price: 1000,
     image: 'animal2.png',
     category: 'Animals',
@@ -66,8 +71,9 @@ export const products = [
   },
   {
     id: 8,
-    name: 'Animal Painting',
-    description: '',
+    name: 'Celestial Falcon – The Sky Wanderer',
+    description:
+      'A fantastical bird-like creature, with radiant wings and glowing eyes, soaring through the night sky. Its beauty represents freedom and the endless journey across the universe.',
     price: 1000,
     image: 'animal3.png',
     category: 'Animals',
@@ -75,8 +81,9 @@ export const products = [
   },
   {
     id: 9,
-    name: 'Animal Painting',
-    description: '',
+    name: 'Elemental Dragon – Fire and Ice',
+    description:
+      'A fierce dragon with a balance of contrasting elements, its glowing eyes reflect the raw power of fire and ice. This creature represents the mastery of both destruction and creation.',
     price: 1000,
     image: 'animal4.png',
     category: 'Animals',
@@ -84,8 +91,9 @@ export const products = [
   },
   {
     id: 10,
-    name: 'Animal Painting',
-    description: '',
+    name: 'Mystic Wolf – The Dream Walker',
+    description:
+      'A magical, swirling wolf-like figure with glowing patterns and mesmerizing eyes. This animal symbolizes a deep connection with dreams, spirituality, and the mysteries of the unknown.',
     price: 1000,
     image: 'animal5.png',
     category: 'Animals',
@@ -138,8 +146,9 @@ export const products = [
   },
   {
     id: 16,
-    name: 'Citys Painting',
-    description: '',
+    name: 'Neon Metropolis – The Pulse of the Future',
+    description:
+      'A vibrant, neon-lit cityscape bustling with advanced technology and dynamic streets. The atmosphere is full of energy, with flying vehicles and glowing architecture, embodying the pulse of a futuristic world.',
     price: 1000,
     image: 'citys1.png',
     category: 'Citys',
@@ -147,8 +156,9 @@ export const products = [
   },
   {
     id: 17,
-    name: 'Citys Painting',
-    description: '',
+    name: 'Skyline of Tomorrow – The Urban Utopia',
+    description:
+      ' A stunning view of a futuristic city filled with towering skyscrapers, advanced transportation systems, and green spaces. This vision of tomorrow blends nature and technology, creating a harmonious urban environment.',
     price: 1000,
     image: 'citys2.png',
     category: 'Citys',
@@ -156,8 +166,9 @@ export const products = [
   },
   {
     id: 18,
-    name: 'Citys Painting',
-    description: '',
+    name: 'Cyber City – A Hub of Innovation',
+    description:
+      'A cyberpunk-inspired metropolis, with illuminated streets, futuristic vehicles, and dynamic energy flowing through every corner. The city is alive with the vibrancy of technology and progress, ready to face the challenges of the future.',
     price: 1000,
     image: 'citys3.png',
     category: 'Citys',
@@ -165,8 +176,9 @@ export const products = [
   },
   {
     id: 19,
-    name: 'Citys Painting',
-    description: '',
+    name: 'Futuristic Oasis – A Green City of Tomorrow',
+    description:
+      'A futuristic city where green spaces, trees, and technology coexist. The streets are lined with lush plants, offering a serene contrast to the bustling energy of the city, symbolizing the harmony between nature and urban life.',
     price: 1000,
     image: 'citys4.png',
     category: 'Citys',
@@ -174,8 +186,9 @@ export const products = [
   },
   {
     id: 20,
-    name: 'Citys Painting',
-    description: '',
+    name: 'Hypercity – Speed and Innovation',
+    description:
+      ' A high-speed futuristic city where cars zoom along vibrant, glowing roads, and flying vehicles navigate through the skyscrapers. This city represents rapid technological advancement and the future of transportation.',
     price: 1000,
     image: 'citys5.png',
     category: 'Citys',
@@ -228,46 +241,51 @@ export const products = [
   },
   {
     id: 26,
-    name: 'Mask Painting',
-    description: '',
+    name: 'The Sun Gods Guardian',
+    description:
+      'A powerful figure wears a sun-shaped mask with glowing tribal patterns across the body. Fiery energy radiates from them, as if the sun itself has chosen its warrior. The character is a mix of ancient power and spiritual authority.',
     price: 1000,
-    image: '/assets/images/products/abstract/mask1.png',
+    image: 'mask1.png',
     category: 'Mask',
     stock: 10, // Antal produkter i lager
   },
   {
     id: 27,
-    name: 'Mask Painting',
-    description: '',
+    name: 'The Neon-Clad Warrior',
+    description:
+      'A warrior in a chrome mask and armor inspired by traditional samurai, but with a high-tech twist. Neon blue lines pulse across the suit. A silent protector from the future – cold, focused, and deadly elegant.',
     price: 1000,
-    image: '/assets/images/products/abstract/mask2.png',
+    image: 'mask2.png',
     category: 'Mask',
     stock: 10, // Antal produkter i lager
   },
   {
     id: 28,
-    name: 'Mask Painting',
-    description: '',
+    name: 'The Forest Guardian',
+    description:
+      'A creature from the depths of the forest, with a mask made of bark and wooden horns. The eyes glow green, and the body is covered in leaves, moss, and mysterious symbols. A being who watches over the balance and life of nature.',
     price: 1000,
-    image: '/assets/images/products/abstract/mask3.png',
+    image: 'mask3.png',
     category: 'Mask',
     stock: 10, // Antal produkter i lager
   },
   {
     id: 29,
-    name: 'Mask Painting',
-    description: '',
+    name: 'The Spirit of Ceremony',
+    description:
+      'Wears a geometric mask in red, black, and gold. The attire is ritualistic and magnificent, filled with symbols from a rich tradition. A spiritual leader, as much a living symbol as a character – dignity and history in one.',
     price: 1000,
-    image: '/assets/images/products/abstract/mask4.png',
+    image: 'mask4.png',
     category: 'Mask',
     stock: 10, // Antal produkter i lager
   },
   {
     id: 30,
-    name: 'Mask Painting',
-    description: '',
+    name: 'The Star Being',
+    description:
+      'A creature from the world of the stars. The mask resembles a crescent moon, and the clothes are like flowing star dust. The skin shimmers in galaxy colors, and a glow of constellations hovers around the head. Mysterious, timeless, and beautiful – a being that moves between dream and reality.',
     price: 1000,
-    image: '/assets/images/products/abstract/mask5.png',
+    image: 'mask5.png',
     category: 'Mask',
     stock: 10, // Antal produkter i lager
   },
